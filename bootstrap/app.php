e269c7c1d1862e979c4b8a526e3037a733a5df8c
BagasBerlian <indonesia4gaming@gmail.com>
2026-06-16 19:23:57 +0700

    feat: configure custom storage and bootstrap paths for Vercel serverless deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__));
+
+if (isset($_ENV['VERCEL']) || getenv('VERCEL')) {
+    $app->useBootstrapPath('/tmp/bootstrap');
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
